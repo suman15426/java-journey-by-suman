735f3e4e4d3e8cdc8072be8b82e235b67bfb8d24
suman15426 <sumanpillala97@gmail.com>
2025-09-01 10:03:31 +0530

    code added successfully here

---
+++ DAY1_varibales_Code.java
@@ -0,0 +1,13 @@
+class Static{
+	 static int a= 25;
+	 static double d= 45.8;
+	 }
+
+class StaticCall{
+
+public static void main(String args[]){
+
+System.out.println(Static.a);
+System.out.println(Static.d);
+}
+}
